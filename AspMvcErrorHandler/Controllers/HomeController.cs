c4336c0707374b4b867809898ce70b016efc00bc
Mikael Chudinov <mikael@chudinov.net>
2016-03-03 13:33:17 +0100

    Rename project.

---
+++ AspMvcErrorHandler/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcErrorHandler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return DataProvider.GetData();
+        }
+    }
+}
